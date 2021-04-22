cb87db02f01d3de0644caabe25b3deadb07efc40
Douglas Daniel Dutra Lopes <douglas.ddlopes@gmail.com>
2021-04-22 09:12:57 -0300

    chore(sizedBox): alter props optional

--- src/components/SizedBox/index.tsx
+++ src/components/SizedBox/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { View } from 'react-native';
 
 interface SizedProps {
-  height: number,
-  width: number
+  height?: number,
+  width?: number
 }
 
-export function SizedBox({ height, width }: SizedProps) {
+export function SizedBox({ height = 0, width = 0 }: SizedProps) {
   return <View style={{ height, width }} />
 }
