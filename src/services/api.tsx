c18ab483c3b41638c596e504e75cd38e549c8d74
Douglas Daniel Dutra Lopes <douglas.ddlopes@gmail.com>
2021-04-22 23:29:34 -0300

    build:(api) add fetch plants e environments

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -5,7 +5,12 @@ const api = axios.create({
 
 export default {
   fetchEnvironment: async () => {
-    const { data } = await api.get('plants_environments');
+    const { data } = await api.get('plants_environments?_sort=title&_order=asc');
+    return data;
+  },
+
+  fetchPlants: async (page: number) => {
+    const { data } = await api.get(`plants?_sort=name&_order=asc&_page=${page}&_limit=8`);
     return data;
   }
 }
