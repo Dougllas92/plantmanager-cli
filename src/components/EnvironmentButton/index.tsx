d62d65d5f6c317f3d0791cdf7e648e9a682e99b8
Douglas Daniel Dutra Lopes <douglas.ddlopes@gmail.com>
2021-04-22 23:31:23 -0300

    build: add photo svg

--- src/components/EnvironmentButton/index.tsx
+++ src/components/EnvironmentButton/index.tsx
@@ -37,7 +37,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 12,
-    marginRight: 5
+    marginHorizontal: 5
   },
   containerActive: {
     backgroundColor: theme.colors.green_light
