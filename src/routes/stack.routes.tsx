ee7820dcd3ae529efa97c6bd00a694b032d4425c
Douglas Daniel Dutra Lopes <douglas.ddlopes@gmail.com>
2021-04-23 22:47:58 -0300

    feat: add route PlantSave

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -6,6 +6,7 @@ import { Welcome } from '../screens/Welcome/Welcome';
 import { UserIdentification } from '../screens/UserIdentification';
 import { Confirmation } from '../screens/Confirmation';
 import { PlantsSelect } from '../screens/PlantsSelect';
+import { PlantSave } from '../screens/PlantSave';
 
 const stackRoutes = createStackNavigator();
 
@@ -22,6 +23,7 @@ const AppRoute: React.FC = () => (
     <stackRoutes.Screen name="UserIdentification" component={UserIdentification} />
     <stackRoutes.Screen name="Confirmation" component={Confirmation} />
     <stackRoutes.Screen name="PlantsSelect" component={PlantsSelect} />
+    <stackRoutes.Screen name="PlantSave" component={PlantSave} />
   </stackRoutes.Navigator>
 )
 
