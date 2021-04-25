50d9e2634e633196fe06998fa4591c13e01f6f6e
Douglas Daniel Dutra Lopes <douglas.ddlopes@gmail.com>
2021-04-25 19:23:33 -0300

    build: configure and createChannel to push notification

--- index.js
+++ index.js
@@ -2,8 +2,36 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import PushNotification from 'react-native-push-notification';
+import { Platform } from 'react-native';
+
+PushNotification.configure({
+  onRegister: function (token) {
+    console.log("TOKEN:", token);
+  },
+
+  onNotification: function (notification) {
+    console.log("NOTIFICATION:", notification);
+  },
+
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+
+  popInitialNotification: true,
+  requestPermissions: Platform.OS === 'ios',
+});
+
+PushNotification.createChannel(
+  {
+    channelId: "plantmanager",
+    channelName: "Plant Manager",
+  },
+  (created) => console.log(`createChannel returned '${created}'`));
 
 AppRegistry.registerComponent(appName, () => App);
