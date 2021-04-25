7f4a898a5f2146839400a913780b61d8f9ffd5d5
Douglas Daniel Dutra Lopes <douglas.ddlopes@gmail.com>
2021-04-25 14:20:06 -0300

    feat: alter function navigate add params

--- src/screens/UserIdentification/index.tsx
+++ src/screens/UserIdentification/index.tsx
@@ -43,9 +43,19 @@ export function UserIdentification() {
     if (!name)
       return Alert.alert('Me diz como podemos chamar você 😢');
 
-    await AsyncStorage.setItem('@plantmanager:user', name);
 
-    navigation.navigate('Confirmation');
+    try {
+      await AsyncStorage.setItem('@plantmanager:user', name);
+      navigation.navigate('Confirmation', {
+        title: 'Prontinho',
+        subtitle: 'Agora vamos começar a cuidar das suas plantinhas com muito cuidado.',
+        buttonTitle: 'Começar',
+        icon: 'smile',
+        nextScreen: 'PlantsSelect'
+      });
+    } catch {
+      Alert.alert('Não foi possivel salvar o seu nome. 😥');
+    }
   }
 
   return (
