510f6f594bd64fb5adccc9f6e4df35a5f52ed5d4
Douglas Daniel Dutra Lopes <douglas.ddlopes@gmail.com>
2021-04-27 10:10:00 -0300

    build: add typescript react-native-svg-transformer

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -12,3 +12,9 @@ declare module '*.json' {
   const content: any;
   export default content;
 }
+
+declare module "*.svg" {
+  import { SvgProps } from "react-native-svg";
+  const content: React.FC<SvgProps>;
+  export default content;
+}
