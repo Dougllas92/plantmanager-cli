c2da674d1d14824e9cc805ab9c223a99095391d6
Douglas Daniel Dutra Lopes <douglas.ddlopes@gmail.com>
2021-04-28 13:23:11 -0300

    refactor: props component to disabled

--- src/components/Button/styles.tsx
+++ src/components/Button/styles.tsx
@@ -1,8 +1,12 @@
 import styled from 'styled-components/native';
 import theme from '../../styles/theme';
 
-export const Btn = styled.TouchableOpacity`
-  background-color: ${theme.colors.green};
+interface ButtonProps {
+  disabled: boolean;
+}
+
+export const Btn = styled.TouchableOpacity<ButtonProps>`
+  background-color: ${props => props.disabled ? theme.colors.green_disable : theme.colors.green};
   height: 56px;
   border-radius: 16px;
   align-items: center;
