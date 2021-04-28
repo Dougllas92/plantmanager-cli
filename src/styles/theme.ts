b88bc98242bdea54d877246b1602c15db69ee084
Douglas Daniel Dutra Lopes <douglas.ddlopes@gmail.com>
2021-04-28 13:48:45 -0300

    style: add color-green-disable and color body to text

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,10 +2,12 @@ const colors = {
   green: '#32B768',
   green_dark: '#2B7A4B',
   green_light: '#DAF2E4',
+  green_disable: '#98dbb3',
 
   heading: '#52665A',
   body_dark: '#738078',
   body_light: '#AAB2AD',
+  body: '#5C6660',
 
   background: '#FFFFFF',
   shape: '#F0F0F0',
